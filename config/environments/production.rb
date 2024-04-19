323084656c0ecc019cd7db5040475676dcb2a15d
riku_nakama <k74rikkin@outlook.com>
2024-04-19 23:12:25 +0900

    Improve production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sample-app-ttxl.onrender.com'
+  host = 'sample-app-ttlx.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
