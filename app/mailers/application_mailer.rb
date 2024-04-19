db8d1ef319120af475d2ab0be6a33d247cafb5fa
riku_nakama <k74rikkin@outlook.com>
2024-04-19 22:36:36 +0900

    Add account activation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "k74rikkin@outlook.com"
   layout "mailer"
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -72,7 +72,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated? should return false for a user with digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 end
 
