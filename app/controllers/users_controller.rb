acf72b0416400e4cd461ab2a8284f56384568102
riku_nakama <k74rikkin@outlook.com>
2024-04-22 20:19:31 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url, status: :see_other and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
+    redirect_to(root_url, status: :see_other) and return unless @user.activated?
   end
 
   def new
@@ -50,14 +51,6 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-    end
-
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
     end
 
     def correct_user
